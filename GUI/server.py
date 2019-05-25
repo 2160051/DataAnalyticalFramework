4c4ab3b1cd7d174155901078631defbc6d425696
Britaknee <32957344+Britaknee@users.noreply.github.com>
2019-05-25 19:33:24 +0800

    Updated server code

--- GUI/server.py
+++ GUI/server.py
@@ -1,4 +1,5 @@
 import eel
+import pandas as pd
 
 eel.init('web')
 
@@ -6,5 +7,13 @@ eel.init('web')
 def test():
     x = '2131231'
     return x
+
+@eel.expose
+def table():
+    df = pd.read_csv('/Users/britanny/Documents/School Files/Thesis/Framework/Data Analytics.csv')
+    columns = df.columns.values
+    csv = pd.read_csv('/Users/britanny/Documents/School Files/Thesis/Framework/Data Analytics.csv', names=columns)
+    tabledata = csv.to_html()
+    return('"""'+ tabledata +'"""')
 
 eel.start('main.html', size=(1920, 1080))
